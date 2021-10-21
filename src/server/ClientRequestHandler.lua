5185b3326dd6b7ec42ff999b8b3fdb40cb810e13
Chipio Industries <chipioindustries@gmail.com>
2021-10-21 12:46:34 -0700

    Update ClientRequestHandler.lua

--- src/server/ClientRequestHandler.lua
+++ src/server/ClientRequestHandler.lua
@@ -1,6 +1,13 @@
+local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local MatchmakingProcessor = require(ServerScriptService.MatchmakingProcessor)
+local PlayerTeleportHandler =  require(ServerScriptService.PlayerTeleportHandler)
+local GetServerType = require(ServerScriptService.GetServerType)
 
 local CONFIG = require(ReplicatedStorage.CONFIG)
+local Enums = require(ReplicatedStorage.Enums)
 
 local QUEUE_ENTRY_LIFETIME = CONFIG.MATCHMAKING_QUEUE.QUEUE_ENTRY_LIFETIME
 
@@ -8,18 +15,60 @@ local remotes = ReplicatedStorage.Remotes
 local addToMatchmaking = remotes.AddToMatchmaking
 local endMatch = remotes.endMatch
 
+local serverType = GetServerType()
+
+local matchmakingCache = {}
+local endMatchVotes = {}
+
 local ClientRequestHandler = {}
 
-function ClientRequestHandler:requestAddToMatchmaking()
+local function checkMatchEndVotes()
+	local voteCount = 0
+	local players = Players:GetPlayers()
 
+	for _, player in pairs(players) do
+		if endMatchVotes[player.UserId] then
+			voteCount += 1
+		end
+	end
+
+	if voteCount > #players / 2 then
+		PlayerTeleportHandler:endMatch()
+	end
 end
 
-function ClientRequestHandler:requestEndMatch()
+function ClientRequestHandler:requestAddToMatchmaking(player)
+	local userId = player.UserId
+	if serverType == Enums.ServerType.Lobby then
+		local timestamp = matchmakingCache[userId]
+		if tick() - timestamp > QUEUE_ENTRY_LIFETIME then
+			local result = MatchmakingProcessor:addPlayer(userId)
+			if result then
+				matchmakingCache[player] = tick()
+			end
+		end
+	end
+end
 
+function ClientRequestHandler:requestEndMatch(player)
+	local userId = player.UserId
+	if serverType == Enums.ServerType.Match then
+		if not endMatchVotes[userId] then
+			endMatchVotes[userId] = true
+			checkMatchEndVotes()
+		end
+	end
 end
 
 function ClientRequestHandler:init()
+	addToMatchmaking.OnServerEvent:Connect(self.requestAddToMatchmaking)
+	endMatch.OnServerEvent:Connect(self.requestEndMatch)
 
+	Players.PlayerRemoving:Connect(function(player)
+		local userId = player.UserId
+		matchmakingCache[userId] = nil
+		endMatchVotes[userId] = nil
+	end)
 end
 
 return ClientRequestHandler
